4f00d3552efd2c13b585d07708e7b8c6bcc27774
Alex Lein <alein@trakitwireless.com>
2024-09-10 16:42:13 -0400

    Reseller deleted!

---
+++ objects/Company/CompanyResellerDeleted.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace trakit.objects {
+	/// <summary>
+	/// Defines the seller company's details for white-labelling.
+	/// </summary>
+	public class CompanyResellerDeleted : Subscribable, IIdUlong, IAmCompany, IDeletable {
+		/// <summary>
+		/// Unique identifier of the Company.
+		/// </summary>
+		/// <seealso cref="Company.id" />
+		public ulong id { get; set; }
+		/// <summary>
+		/// The unique identifier of this company's parent organization.
+		/// </summary>
+		/// <seealso cref="Company.id" />
+		public ulong parent { get; set; }
+		/// <summary>
+		/// This flag is always true.
+		/// </summary>
+		public bool deleted => true;
+		/// <summary>
+		/// Timestamp from the action that deleted this asset.
+		/// </summary>
+		public DateTime since { get; set; }
+	}
+}
