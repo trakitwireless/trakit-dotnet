e2b35327aaa76ce3a20eeae90e8d6046a4b67866
Alex Lein <alein@trakitwireless.com>
2024-09-13 14:10:34 -0400

    Moved some files around

--- objects/API/Timezone.cs
+++ objects/API/Timezone.cs
@@ -24,20 +24,4 @@
 		/// <override readonly="true" />
 		public bool dst;
 	}
-
-	/// <summary>
-	/// Part of the White-labelling profile definitions.
-	/// </summary>
-	public class ColourStyle {
-		/// <summary>
-		/// The colour of the background.
-		/// </summary>
-		/// <override max-length="22" format="colour" />
-		public string fill;
-		/// <summary>
-		/// The colour of the text or outline.
-		/// </summary>
-		/// <override max-length="22" format="colour" />
-		public string stroke;
-	}
 }
---
+++ objects/Company/ColourStyle.cs
@@ -0,0 +1,17 @@
+﻿namespace trakit.objects {
+	/// <summary>
+	/// Part of the White-labelling profile definitions.
+	/// </summary>
+	public class ColourStyle {
+		/// <summary>
+		/// The colour of the background.
+		/// </summary>
+		/// <override max-length="22" format="colour" />
+		public string fill;
+		/// <summary>
+		/// The colour of the text or outline.
+		/// </summary>
+		/// <override max-length="22" format="colour" />
+		public string stroke;
+	}
+}
