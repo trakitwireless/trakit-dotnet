9ca5c691388a048e7defdd609dafdac3dc64b1ea
Alex Lein <alein@trakitwireless.com>
2024-09-18 16:33:32 -0400

    Updated Interfaces

--- objects/_common/Interfaces.cs
+++ objects/_common/Interfaces.cs
@@ -18,12 +18,14 @@ namespace trakit.objects {
 	public interface IDeletable {
 		/// <summary>
 		/// Marked as true for objects that have been deleted.
+		/// This value is not present in the JSON scheme when <see cref="deleted"/> is false.
 		/// </summary>
-		bool deleted { get; }
+		bool? deleted { get; }
 		/// <summary>
 		/// Timestamp from the action that deleted this object.
+		/// This value is not present in the JSON scheme when <see cref="deleted"/> is false.
 		/// </summary>
-		DateTime since { get; }
+		DateTime? since { get; }
 	}
 	/// <summary>
 	/// An interface for objects that can be marked as "enabled".
@@ -42,12 +44,14 @@ namespace trakit.objects {
 	public interface ISuspendable {
 		/// <summary>
 		/// Marked as true for objects that have been suspended.
+		/// This value is not present in the JSON scheme when <see cref="deleted"/> is false.
 		/// </summary>
-		bool suspended { get; }
+		bool? suspended { get; }
 		/// <summary>
 		/// A timestamp from when the object was most recently suspended or revived.
+		/// This value is not present in the JSON scheme when <see cref="suspended"/> is false.
 		/// </summary>
-		DateTime since { get; }
+		DateTime? since { get; }
 	}
 	/// <summary>
 	/// An interface for objects that can be marked as "global".
