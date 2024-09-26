148998062bcc24617a1ee78d1912277ba8e55088
Alex Lein <alein@trakitwireless.com>
2024-09-26 16:21:50 -0400

    Added nuget for JSON Added responses for listing assets by company

---
+++ objects/API/Responses/RespListAssetBy.cs
@@ -0,0 +1,11 @@
+﻿namespace trakit.objects {
+	/// <summary>
+	/// A container for the id and owning <see cref="Company.id"/> of the object requested/created.
+	/// </summary>
+	public class RespListAssetBy : ResponseType {
+		/// <summary>
+		/// The list of requested <see cref="Asset"/>s.
+		/// </summary>
+		public Asset[] assets;
+	}
+}
