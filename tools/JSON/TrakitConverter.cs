5433c5191f82242d2dadd6fdf37fab70f69ae8c6
Alex Lein <alein@trakitwireless.com>
2024-10-02 14:15:23 -0400

    Fixed prod URL ctor Updated read helper for each of use... I think.

--- tools/JSON/TrakitConverter.cs
+++ tools/JSON/TrakitConverter.cs
@@ -9,6 +9,50 @@ namespace trakit.tools {
 	/// </summary>
 	public abstract class TrakitConverter<T> : JsonConverter<T> where T : Subscribable {
 
+		protected JsonNode read(ref Utf8JsonReader reader) {
+			JsonNode node = null;
+			while (reader.Read()) {
+				switch (reader.TokenType) {
+					case JsonTokenType.None:
+					case JsonTokenType.Null:
+						break;
+					case JsonTokenType.StartObject:
+						node = this.readObject(ref reader);
+						break;
+					case JsonTokenType.StartArray:
+						node = this.readArray(ref reader);
+						break;
+					case JsonTokenType.String:
+						node = reader.GetString();
+						break;
+					case JsonTokenType.Number:
+						if (reader.TryGetInt64(out long l)) node = l;
+						if (reader.TryGetDouble(out double d)) node = d;
+						break;
+					case JsonTokenType.True:
+					case JsonTokenType.False:
+						node = reader.GetBoolean();
+						break;
+					default:
+						throw new JsonException();
+				}
+			}
+			return node;
+		}
+		protected JsonArray readArray(ref Utf8JsonReader reader) {
+			if (reader.TokenType != JsonTokenType.StartArray) throw new JsonException();
+			var obj = new JsonArray();
+			while (reader.Read()) {
+				if (reader.TokenType == JsonTokenType.EndArray) break;
+
+				string key = reader.GetString();
+				reader.Read();
+				obj.Add(key, JsonNode.Parse(ref reader));
+			}
+			return obj;
+		}
+
+
 		protected JsonObject readObject(ref Utf8JsonReader reader) {
 			if (reader.TokenType != JsonTokenType.StartObject) throw new JsonException();
 			var obj = new JsonObject();
