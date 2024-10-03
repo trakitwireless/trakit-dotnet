3e5e1346e88d3a0d2d22c364cabe36fba3746433
Alex Lein <alein@trakitwireless.com>
2024-10-03 14:10:44 -0400

    Partially completed marking things for listing by RESTful service Added new interface to include ProviderRegistrations

---
+++ objects/_common/Interfaces/IRequestable.cs
@@ -0,0 +1,12 @@
+﻿namespace trakit.objects {
+	/// <summary>
+	/// The main interface for an object in the Trak-iT system.
+	/// </summary>
+	public interface IRequestable {
+		/// <summary>
+		/// Returns a unique identifier as a string.
+		/// </summary>
+		/// <returns></returns>
+		string getKey();
+	}
+}
--- objects/_common/Subscribable.cs
+++ objects/_common/Subscribable.cs
@@ -2,10 +2,23 @@
 	/// <summary>
 	/// Any derived class can/should be serialized and given to a user.
 	/// </summary>
-	public abstract class Subscribable {
+	public abstract class Subscribable : IRequestable {
 		/// <summary>
 		/// Object version keys used to validate synchronization for all object properties.
 		/// </summary>
 		public uint[] v;
+		/// <summary>
+		/// Returns a string which can be used as a unique identifier for this object.
+		/// Strings are unique for each type of object, but can be identical for different object types.
+		/// </summary>
+		/// <seealso cref="IIdUlong.id"/>
+		/// <seealso cref="Machine.key"/>
+		/// <seealso cref="Provider.id"/>
+		/// <seealso cref="ProviderRegistration.code"/>
+		/// <seealso cref="User.login"/>
+		/// <seealso cref="UserGeneral.login"/>
+		/// <seealso cref="UserAdvanced.login"/>
+		/// <returns>A string unique for this type of object.</returns>
+		public abstract string getKey();
 	}
 }
