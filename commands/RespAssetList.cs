d5fba245d62d2c3675ced5cd6849254e9b54da30
Alex Lein <alein@trakitwireless.com>
2024-10-08 17:55:29 -0400

    Standard asset listing response

---
+++ commands/RespAssetList.cs
@@ -0,0 +1,17 @@
+﻿using trakit.objects;
+
+namespace trakit.commands {
+	/// <summary>
+	/// 
+	/// </summary>
+	public class RespAssetList : ResponseType {
+		/// <summary>
+		/// 
+		/// </summary>
+		public RespId company;
+		/// <summary>
+		/// 
+		/// </summary>
+		public Asset[] assets;
+	}
+}
